f86389843d4f70cb2db81e6eb5006d82d2922a52
root <root@38ZPMG3.dirsvcs.org>
2022-10-12 16:36:54 -0500

    user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_10_12_193225) do
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "email"
+    t.string "password"
+  end
+
 end
