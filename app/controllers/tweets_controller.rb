aa7e13885e3a27fd9313c379f4b64c9f47b7c621
root <root@38ZPMG3.dirsvcs.org>
2022-10-13 15:28:25 -0500

    sessions/tweets/user controllers

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,10 @@
 class TweetsController < ApplicationController
+    def index
+        @tweets = Tweet.all
+        remder 'tweets/index'
+    end
+
+    def create
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,18 @@ Rails.application.routes.draw do
   get '/feeds' => 'feeds#index'
 
   # USERS
+  post '/users' => 'users#create'
 
   # SESSIONS
+  post '/sessions' => 'sessions#create'
+  get '/authenticated' => 'sessions#authenticated'
+  delete '/sessions' => 'sessions#destroy'
 
   # TWEETS
+  post '/tweets' => 'tweets#create'
+  get '/tweets' => 'tweets#index'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
+
 end
