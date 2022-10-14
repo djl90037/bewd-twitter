ac4f8f132f4255d6cafb8d47072e37cd258e9f6c
daniel <djl90037@gmail.com>
2022-10-13 20:27:38 -0500

    tweets controller WIP

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,26 @@ class TweetsController < ApplicationController
     end
 
     def create
-        
+        token = cookies.permanent.signed[:twitter_session_token]
+        session = Session.find_by(token: token)
+
+        if session
+            user = session.user
+            @tweet = user.tweets.new(tweet_params)
+
+            if @tweet.save
+                render 'tweets/create'
+            else
+                render json: { success: false }
+            end
+        else
+            render json: { success: false }
+        end
+    end
+
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:message)
     end
 end
